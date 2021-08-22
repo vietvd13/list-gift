98c76abf7fb2d15048deedd648a21ab7e582ecfa
vietvd13 <vuducviet0131@gmail.com>
2021-08-22 17:08:42 +0700

    deploy

---
+++ precache-manifest.a58d8634f755b9994a0cc3be67059acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45d48bc1c022596bcd1",
+    "url": "/list-gift/css/app.8752d0cb.css"
+  },
+  {
+    "revision": "aee5799ea6ed94d5211c",
+    "url": "/list-gift/css/chunk-vendors.f4e10491.css"
+  },
+  {
+    "revision": "019f2b27abbc5aa9708d85e1feb3edaa",
+    "url": "/list-gift/index.html"
+  },
+  {
+    "revision": "a45d48bc1c022596bcd1",
+    "url": "/list-gift/js/app.365facdb.js"
+  },
+  {
+    "revision": "aee5799ea6ed94d5211c",
+    "url": "/list-gift/js/chunk-vendors.4f1265c2.js"
+  },
+  {
+    "revision": "e982daf7326339601dc8ca8dc52b2be7",
+    "url": "/list-gift/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/list-gift/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/list-gift/precache-manifest.a58d8634f755b9994a0cc3be67059acb.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "list-gift"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
